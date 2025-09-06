f3ba31cc2eeac4c68c3e143e997583e1fa16f898
Daniel Camuçatto <daniel.camucatto@gmail.com>
2025-09-05 23:48:28 -0300

    feat: add initial implementation of Mario pyramid in C and pseudocode documentation

---
+++ cs50/ex-mario/ex-mario.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Em um arquivo chamado mario.c, vamos implementar um programa em C que recrie essa pirâmide usando hashes ( # ) para os tijolos, como no exemplo a seguir:
+
+//               #
+//             ##
+//           ###
+//         ####
+//       #####
+//     ######
+//  #######
+// ########
+
+int main(void){
+
+    
+}
---
+++ cs50/ex-mario/pseudcodigo.md
@@ -0,0 +1,15 @@
+# Exercicio 01 CS50 Mario 
+## Implementando psseudo-codigo
+
+Em um arquivo chamado mario.c, vamos implementar um programa em C que recrie essa pirâmide usando hashes ( # ) para os tijolos, como no exemplo a seguir:
+
+              #
+            ##
+          ###
+        ####
+      #####
+    ######
+ #######
+########
+
+- pedir ao usuário 
